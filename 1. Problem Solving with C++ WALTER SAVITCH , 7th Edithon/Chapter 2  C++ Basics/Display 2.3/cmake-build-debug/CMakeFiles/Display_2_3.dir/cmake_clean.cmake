bbed5c70eaade8cfb1ee2c56ee8caaf83153beac
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-06-28 16:09:06 +0000

    Adding Display 2.3, chapter 2.

---
+++ 1. Problem Solving with C++ WALTER SAVITCH , 7th Edithon/Chapter 2  C++ Basics/Display 2.3/cmake-build-debug/CMakeFiles/Display_2_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Display_2_3.dir/main.cpp.o"
+  "Display_2_3.pdb"
+  "Display_2_3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Display_2_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
