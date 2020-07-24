ed7b4dc2a11ef74b7decae4f5c35e3999258f4bc
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-24 06:25:05 +0000

    Implementing pointers 2.Working, chap01, lynda

--- Lynda C++ Essential Training/Chap01/2. Working/main.cpp
+++ Lynda C++ Essential Training/Chap01/2. Working/main.cpp
@@ -5,9 +5,16 @@ int main() {
     //int x;
     //x = 42;
 
-    const int i = 7;
+    //const int i = 7;
+
+    int x = 7;
+    int y = 42;
+    int *ip = &x;
+    printf("x is %d\n", x);
+    printf("y is %d\n", y);
+    printf("ip is %d\n", *ip);
 
     // %d is a token for printf where it puts the integer value
-    printf("I is %d\n", i);
+    //printf("I is %d\n", i);
     return 0;
 }
