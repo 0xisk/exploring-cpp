62009f293f3ba8b53bd126b640d3b2bb7b62283f
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-24 15:03:22 +0000

    Implementing arrays and strings 2.Working, chap01, lynda.

--- Lynda C++ Essential Training/Chap01/2. Working/main.cpp
+++ Lynda C++ Essential Training/Chap01/2. Working/main.cpp
@@ -7,18 +7,39 @@ int main() {
 
     //const int i = 7;
 
-    int x = 7;
-    int *ip = &x;
+    //int x = 7;
+    //int *ip = &x;
 
     // Reference
     // References are immutable cannot change it
-    int &y = x;
+    //int &y = x;
 
-    printf("x is %d\n", x);
-    printf("y is %d\n", y);
-    printf("ip is %d\n", *ip);
+    //printf("x is %d\n", x);
+    //printf("y is %d\n", y);
+    //printf("ip is %d\n", *ip);
 
     // %d is a token for printf where it puts the integer value
     //printf("I is %d\n", i);
+
+    char s[] = { 's', 't', 'r', 'i', 'n', 'g' };
+    char a[] = "Array 2";
+
+    printf("String is: %s\n", s);
+
+    for(int i = 0; s[i]; ++i) {
+        //printf("String is: %c\n", s[i]);
+    }
+
+    for(char *cp = s; *cp; ++cp) {
+        //printf("String is: %c\n", *cp);
+    }
+
+    for(char c : s) {
+        if(c == 0) break;
+        printf("String is: %c\n", c);
+    }
+
+    //printf("String is: %s\n", s2);
+
     return 0;
 }
