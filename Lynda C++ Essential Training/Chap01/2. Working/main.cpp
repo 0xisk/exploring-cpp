be64f8e284ad206aed9ab8a9c9636304876ccbce
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-24 06:05:23 +0000

    Implementing 2.Working, chap01, lynda

---
+++ Lynda C++ Essential Training/Chap01/2. Working/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    x = 42;
+
+    // %d is a token for printf where it puts the integer value
+    printf("X is %d\n", x);
+    return 0;
+}
