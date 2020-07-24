a20d8093c32356c888447e793cd2264ca2f13db3
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-24 05:58:59 +0000

    Implementing a Hello, chap01, lynda.

---
+++ Lynda C++ Essential Training/Chap01/1. Hello/cmake-build-debug/CMakeFiles/1__Hello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1__Hello"
+  "1__Hello.pdb"
+  "CMakeFiles/1__Hello.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1__Hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lynda C++ Essential Training/Chap01/1. Hello/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    puts("Hello World!!");
+    printf("Hello World!!!!");
+    return 0;
+}
