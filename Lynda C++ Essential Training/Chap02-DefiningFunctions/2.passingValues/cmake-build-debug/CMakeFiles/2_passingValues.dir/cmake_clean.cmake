dd605b69ad7b2cc43a0ff062c3cca21155ecea37
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-31 09:33:01 +0000

    Making new project for passing values to function

---
+++ Lynda C++ Essential Training/Chap02-DefiningFunctions/2.passingValues/cmake-build-debug/CMakeFiles/2_passingValues.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "2_passingValues"
+  "2_passingValues.pdb"
+  "CMakeFiles/2_passingValues.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_passingValues.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
