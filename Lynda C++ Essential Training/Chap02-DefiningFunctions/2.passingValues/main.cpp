bce27282eec2c8b6011934323c7941500e32ab41
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-31 09:39:08 +0000

    Implementing passing by reference

--- Lynda C++ Essential Training/Chap02-DefiningFunctions/2.passingValues/main.cpp
+++ Lynda C++ Essential Training/Chap02-DefiningFunctions/2.passingValues/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
 
-void func(int i) {
+void func(int &i) {
+    i = 73;
     printf("Value is %d\n", i);
 }
 
 int main() {
+    int x = 42;
     puts("This is main()");
-    func(33); // Pass by value
+    func(x); // Pass by value
+    printf("X is %d\n", x);
     return 0;
 }
