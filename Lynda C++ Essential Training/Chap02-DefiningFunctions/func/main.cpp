fb5d32e481ff25682a4d951ed83734f34d489f89
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-31 09:27:25 +0000

    Implementing the Forward declaration

--- Lynda C++ Essential Training/Chap02-DefiningFunctions/func/main.cpp
+++ Lynda C++ Essential Training/Chap02-DefiningFunctions/func/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
 
-void func() {
-    puts("This is func()");
-}
+// Forward declaration
+void func();
 
 int main() {
     puts("This is main()");
     func();
     return 0;
+}
+
+void func() {
+    puts("This is func()");
 }
