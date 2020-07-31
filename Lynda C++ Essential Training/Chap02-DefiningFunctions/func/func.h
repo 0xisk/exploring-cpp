e01f963153e2a31f8e375071c12a2ec40eacc7fb
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-31 09:29:51 +0000

    Implementing a Header file

---
+++ Lynda C++ Essential Training/Chap02-DefiningFunctions/func/func.h
@@ -0,0 +1,10 @@
+//
+// Created by iskander on 7/31/20.
+//
+
+#ifndef DEFININGFUNCTIONS_FUNC_H
+#define DEFININGFUNCTIONS_FUNC_H
+
+void func();
+
+#endif //DEFININGFUNCTIONS_FUNC_H
--- Lynda C++ Essential Training/Chap02-DefiningFunctions/func/main.cpp
+++ Lynda C++ Essential Training/Chap02-DefiningFunctions/func/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include "func.h"
 using namespace std;
 
+/*
 // Forward declaration
 void func();
+*/
 
 int main() {
     puts("This is main()");
