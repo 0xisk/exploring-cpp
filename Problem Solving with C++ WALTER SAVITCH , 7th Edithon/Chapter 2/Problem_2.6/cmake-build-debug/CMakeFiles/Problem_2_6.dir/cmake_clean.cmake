0291108d6f2f160705c46c7233b4d609615313d8
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-08-07 08:35:11 +0000

    Initialize a new project for problem_2.5

---
+++ Problem Solving with C++ WALTER SAVITCH , 7th Edithon/Chapter 2/Problem_2.6/cmake-build-debug/CMakeFiles/Problem_2_6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Problem_2_6.dir/main.cpp.o"
+  "Problem_2_6"
+  "Problem_2_6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Problem_2_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
