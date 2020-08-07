b5c5e1dcaeaa62785e23603e899cf318f9649e5a
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-08-07 10:00:31 +0000

    Implementing problem_2.6 chapter 2

--- Problem Solving with C++ WALTER SAVITCH , 7th Edithon/Chapter 2/Problem_2.6/main.cpp
+++ Problem Solving with C++ WALTER SAVITCH , 7th Edithon/Chapter 2/Problem_2.6/main.cpp
@@ -2,6 +2,41 @@
 using namespace std;
 
 int main() {
+    bool contTheProgram = true;
 
+    do {
+        int roomCapacity, numberOfPeople;
+        char userEnter;
+
+        puts("Please enter the meeting room capacity number: ");
+        cin >> roomCapacity;
+
+        puts("Please enter the number of the attendence: ");
+        cin >> numberOfPeople;
+
+        if(numberOfPeople <= roomCapacity) {
+            cout << "You can allow more attendance, about: " << roomCapacity - numberOfPeople << " more!" << endl;
+        } else {
+            cout << "Sorry, according to the fire regulations of the meeting room, it's required to exclude about " << numberOfPeople - roomCapacity << " from the attendance" << endl;
+        }
+
+        bool completed = true;
+
+        do {
+            cout << "Please click Y/N if you want to execute the program again: " << endl;
+            cin >> userEnter;
+
+            if (userEnter == 'y' || userEnter == 'Y') {
+                contTheProgram = true;
+                completed = false;
+            } else if (userEnter == 'n' || userEnter == 'N') {
+                contTheProgram = false;
+                completed = false;
+            } else {
+                cout << "Please enter a valid choice: " << endl;
+            }
+        } while (completed);
+
+    } while (contTheProgram);
     return 0;
 }
